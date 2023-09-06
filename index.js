1249cfe076c7b5c36e305b581824e8891877a470
Carl Angelo Nievarez - A70458 <CarlAngelo.Nievarez@microchip.com>
2023-09-06 09:41:39 +0800

    Initial commit

---
+++ index.js
@@ -0,0 +1,14 @@
+const express = require('express');
+const os = require('os');
+
+const app = express();
+const port = process.env.PORT || 3000;
+
+app.get('/', (req, res) => {
+  const hostname = os.hostname();
+  res.send(`Hosted on: ${hostname}\n`);
+});
+
+app.listen(port, () => {
+  console.log(`Server is running on port ${port}`);
+});
